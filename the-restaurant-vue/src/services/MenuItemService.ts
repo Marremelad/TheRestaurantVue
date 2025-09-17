c66c4e53591e89d076e2d9bd8c69552527ac391d
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-17 15:04:24 +0200

    Created MenuItem service and MenuItem type

---
+++ the-restaurant-vue/src/services/MenuItemService.ts
@@ -0,0 +1,15 @@
+﻿import api from "@/services/api.ts";
+import type { ApiResponse, MenuItem } from "@/types/types.ts";
+
+export const GetMenuItems = async (mi: MenuItem): Promise<ApiResponse<MenuItem[]>> => {
+    const response = await api.get("menu-items", {
+        id: mi.id,
+        name: mi.name,
+        price: mi.price,
+        description: mi.description,
+        image: mi.image,
+        isPopular: mi.isPopular
+    })
+
+    return response.data
+}
--- the-restaurant-vue/src/types/types.ts
+++ the-restaurant-vue/src/types/types.ts
@@ -1,19 +1,28 @@
 ﻿export interface ApiResponse<T = any> {
-    isSuccess: boolean
-    statusCode: number
-    value: T | null
+    isSuccess: boolean,
+    statusCode: number,
+    value: T | null,
     message: string
 }
 
 export interface AvailabilityRequest {
-    partySize: number
+    partySize: number,
     date: string
 }
 
 export interface AvailabilityResponse {
-    date: string
-    timeSlot: number
-    displayableTimeSlot: string
-    tableNumber: number
+    date: string,
+    timeSlot: number,
+    displayableTimeSlot: string,
+    tableNumber: number,
     tableCapacity: number
+}
+
+export interface MenuItem {
+    id: number,
+    name: string,
+    price: number,
+    description: string,
+    image: string,
+    isPopular: boolean
 }
